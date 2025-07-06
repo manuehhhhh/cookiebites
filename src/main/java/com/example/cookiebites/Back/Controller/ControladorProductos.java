4a521f20a6e48cf6c54479f91f003a223efad58a
lausmp <lausofimarper@gmail.com>
2025-07-06 10:56:48 -0400

    editar producto

--- src/main/java/com/example/cookiebites/Back/Controller/ControladorProductos.java
+++ src/main/java/com/example/cookiebites/Back/Controller/ControladorProductos.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -80,5 +81,20 @@ public class ControladorProductos {
         public ResponseEntity<Void> eliminarProducto(@PathVariable String nombre) {
         listaProductos.eliminarProducto(nombre);
         return ResponseEntity.noContent().build();
+    }
+
+    @PutMapping("/productos/editar/{nombre}")
+    public ResponseEntity<Void> editarProducto(@PathVariable String nombre, @RequestBody Producto productoEditado) {
+        Producto productoExistente = listaProductos.consultaProducto(nombre);
+        if (productoExistente != null) {
+            productoExistente.setNombre(productoEditado.getNombre());
+            productoExistente.setPrecio(productoEditado.getPrecio());
+            productoExistente.setIngredientes(productoEditado.getIngredientes());
+            productoExistente.setImagen(productoEditado.getImagen());
+            listaProductos.actualizarProductos(); // Guarda los cambios en el JSON
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
     }
 }
