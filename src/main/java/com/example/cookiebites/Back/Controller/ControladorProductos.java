a5f477fe18833cb07729d5d26536a6d62419888f
lausmp <lausofimarper@gmail.com>
2025-07-07 17:39:07 -0400

    imagenes arregladas

--- src/main/java/com/example/cookiebites/Back/Controller/ControladorProductos.java
+++ src/main/java/com/example/cookiebites/Back/Controller/ControladorProductos.java
@@ -17,10 +17,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 import com.example.cookiebites.Back.Model.Producto;
 import com.example.cookiebites.Back.Repository.ListaProductos;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.multipart.MultipartFile;
 import java.io.File;
-import java.io.IOException;
 
 
 @RestController
